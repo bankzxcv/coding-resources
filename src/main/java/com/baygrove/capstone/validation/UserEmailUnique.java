3aeec24a99efecf548953d5ca86ddd53ee4e88f2
BayLadyCoder <rachada_32@hotmail.com>
2024-07-28 22:56:42 -0400

    Custom UserEmailUnique validation annotation

---
+++ src/main/java/com/baygrove/capstone/validation/UserEmailUnique.java
@@ -0,0 +1,19 @@
+package com.baygrove.capstone.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = UserEmailUniqueImpl.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserEmailUnique {
+    String message() default "{UserEmailUnique}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ src/main/java/com/baygrove/capstone/validation/UserEmailUniqueImpl.java
@@ -0,0 +1,28 @@
+package com.baygrove.capstone.validation;
+
+
+import com.baygrove.capstone.database.dao.UserDAO;
+import com.baygrove.capstone.database.entity.User;
+import jakarta.validation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.util.*;
+
+
+@Slf4j
+public class UserEmailUniqueImpl implements ConstraintValidator<UserEmailUnique, String> {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (StringUtils.isEmpty(value)) {
+            return true;
+        }
+
+        User user = userDAO.findByEmailIgnoreCase(value);
+
+        return (user == null);
+    }
+}
