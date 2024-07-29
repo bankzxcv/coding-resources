9ab2a5ed241dfef044be44aeb4e2bb4f5c055204
BayLadyCoder <rachada_32@hotmail.com>
2024-07-29 17:59:50 -0400

    Add SecurityFilterChain to guard specific routes, override default Log In page, setup Log Out endpoint, and add CSRF middleware.

--- src/main/java/com/baygrove/capstone/config/SpringSecurity.java
+++ src/main/java/com/baygrove/capstone/config/SpringSecurity.java
@@ -2,8 +2,12 @@ package com.baygrove.capstone.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SpringSecurity {
@@ -11,4 +15,30 @@ public class SpringSecurity {
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(Customizer.withDefaults());
+        // http.csrf(csrf -> csrf.disable());
+
+        http.authorizeRequests()
+                .requestMatchers(
+                        new AntPathRequestMatcher("/admin/**"),
+                        new AntPathRequestMatcher("/user/**")).authenticated()
+                .anyRequest().permitAll();
+
+        http.formLogin(formLogin -> formLogin
+                .loginPage("/user/login") // GET
+                .loginProcessingUrl("/user/login") // POST
+        );
+
+        http.logout(formLogout -> formLogout
+                .invalidateHttpSession(true)
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/"));
+
+        return http.build();
+    }
+
 }
