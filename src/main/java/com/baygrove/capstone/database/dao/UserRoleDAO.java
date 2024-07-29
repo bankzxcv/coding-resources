a369b73db308867d307ba34fa14887279889559e
BayLadyCoder <rachada_32@hotmail.com>
2024-07-29 18:07:54 -0400

    Add findByUserId method to UserRoleDAO

--- src/main/java/com/baygrove/capstone/database/dao/UserRoleDAO.java
+++ src/main/java/com/baygrove/capstone/database/dao/UserRoleDAO.java
@@ -4,8 +4,12 @@ package com.baygrove.capstone.database.dao;
 import com.baygrove.capstone.database.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface UserRoleDAO extends JpaRepository<UserRole, Long> {
     UserRole findById(Integer id);
 
+
+    List<UserRole> findByUserId(Integer userId);
 }
