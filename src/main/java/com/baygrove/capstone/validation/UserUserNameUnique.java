353273948f08f9f80b5ec73a9cdcd2d7fb20bbae
BayLadyCoder <rachada_32@hotmail.com>
2024-07-30 15:42:44 -0400

    Create UserUsernameUnique validation annotation

---
+++ src/main/java/com/baygrove/capstone/validation/UserUserNameUnique.java
@@ -0,0 +1,20 @@
+package com.baygrove.capstone.validation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = UserUserNameUniqueImpl.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserUserNameUnique {
+    String message() default "{UserUsernameUnique}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ src/main/java/com/baygrove/capstone/validation/UserUserNameUniqueImpl.java
@@ -0,0 +1,26 @@
+package com.baygrove.capstone.validation;
+
+import com.baygrove.capstone.database.dao.UserDAO;
+import com.baygrove.capstone.database.entity.User;
+import jakarta.validation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.util.*;
+
+@Slf4j
+public class UserUserNameUniqueImpl implements ConstraintValidator<UserUserNameUnique, String> {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext context) {
+        if (StringUtils.isEmpty(username)) {
+            return true;
+        }
+
+        User user = userDAO.findByUsernameIgnoreCase(username);
+
+        return (user == null);
+    }
+}
