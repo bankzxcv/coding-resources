8dd833123f09e799c8e1243ef0be87f7fe11ac76
BayLadyCoder <rachada_32@hotmail.com>
2024-07-31 14:54:57 -0400

    Fix warning

--- src/main/java/com/baygrove/capstone/database/dao/ResourceDAO.java
+++ src/main/java/com/baygrove/capstone/database/dao/ResourceDAO.java
@@ -1,16 +1,15 @@
 package com.baygrove.capstone.database.dao;
 
 import com.baygrove.capstone.database.entity.Resource;
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ResourceDAO extends JpaRepository<Resource, Long> {
     Resource findById(Integer id);
 
-    @Query("from Resource")
-    @Override
+    @Nonnull
     List<Resource> findAll();
 
 }
