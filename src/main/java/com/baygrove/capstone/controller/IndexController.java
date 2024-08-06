53078e195211e1e0b5dae6a47e28e5669a41fd01
BayLadyCoder <rachada_32@hotmail.com>
2024-08-06 17:19:25 -0400

    Provide user list id if logged in

--- src/main/java/com/baygrove/capstone/controller/IndexController.java
+++ src/main/java/com/baygrove/capstone/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.baygrove.capstone.controller;
 
 import com.baygrove.capstone.database.dao.ResourceDAO;
+import com.baygrove.capstone.database.dao.UserListDAO;
 import com.baygrove.capstone.database.entity.Resource;
+import com.baygrove.capstone.database.entity.User;
+import com.baygrove.capstone.database.entity.UserList;
+import com.baygrove.capstone.security.AuthenticatedUserUtilities;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +20,12 @@ public class IndexController {
     @Autowired
     ResourceDAO resourceDAO;
 
+    @Autowired
+    UserListDAO userListDAO;
+
+    @Autowired
+    AuthenticatedUserUtilities authenticatedUserUtilities;
+
     @GetMapping("/")
     public ModelAndView index() {
         ModelAndView response = new ModelAndView("index");
@@ -23,6 +33,13 @@ public class IndexController {
         List<Resource> resources = resourceDAO.findAll();
 
         response.addObject("resources", resources);
+
+        User user = authenticatedUserUtilities.getCurrentUser();
+
+        if (user != null) {
+            List<UserList> userLists = userListDAO.findByUserId(user.getId());
+            response.addObject("userListId", userLists.get(0).getId());
+        }
 
         return response;
     }
