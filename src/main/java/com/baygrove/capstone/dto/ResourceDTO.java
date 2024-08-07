e3962c63dff2c7b9f801b610d4483714d157550b
BayLadyCoder <rachada_32@hotmail.com>
2024-08-06 22:59:55 -0400

    Create ResourceDTO

---
+++ src/main/java/com/baygrove/capstone/dto/ResourceDTO.java
@@ -0,0 +1,42 @@
+package com.baygrove.capstone.dto;
+
+import com.baygrove.capstone.database.entity.ResourceCategory;
+import com.baygrove.capstone.database.entity.ResourceList;
+import com.baygrove.capstone.database.entity.ResourceTopic;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ResourceDTO {
+    private boolean isAdded = false;
+
+    private Integer id;
+
+    private String name;
+
+    private String url;
+
+    private String description;
+
+    private String imageUrl;
+
+    private String status;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private List<ResourceCategory> resourceCategories;
+
+    private List<ResourceTopic> resourceTopics;
+
+    private List<ResourceList> resourceLists;
+}
